bd18a7cd156f42d4eeb6363389290fc5f7560a9f
Kabir <sagithedeveloper@gmail.com>
2023-11-22 16:16:27 +0530

    added the code

---
+++ JSB1/S03_22-11-2023/functions.js
@@ -0,0 +1,18 @@
+function fnAdd() {
+  var x;
+  var n = 100;
+  var y;
+  var c = 100;
+  var result;
+  x = 50;
+  y = 200;
+  result = x + y;
+}
+
+function print() {
+  console.log("This is my second function");
+  console.log("This is my second function second Statement");
+}
+
+fnAdd();
+print();
