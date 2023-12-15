f73486c249cd169bab8349dfce0eead86aeabed5
Kabir <sagithedeveloper@gmail.com>
2023-12-15 16:31:35 +0530

    added the code

---
+++ JSB1/S18_15-12-2023/array.js
@@ -0,0 +1,19 @@
+// var arr = [10, 20, 30, 40];
+
+// var arr = [
+//   [10, 20],
+//   [30, 40],
+//   [50, 60],
+// ];
+var arr = [
+  [10, 20],
+  [30, 40],
+  [50, ["Sneha", "Riya"]],
+];
+
+// console.log(arr[3]);
+
+// console.log(arr[1][1]);
+// console.log(arr[2][0]);
+
+console.log(arr[2][1][1]);
